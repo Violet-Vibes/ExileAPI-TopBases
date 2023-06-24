42805edeb9cf52ddc2b96dc226c3f26c0de69735
Violet Vibes <qsato@protonmail.com>
2023-06-24 12:53:07 +0200

    Upload files

---
+++ TopBases.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Concurrent;
+using ExileCore;
+using ExileCore.PoEMemory.Elements.InventoryElements;
+using ExileCore.Shared.Cache;
+using SharpDX;
+using System.Collections.Generic;
+using ExileCore.Shared.Enums;
+using System.Linq;
+using ImGuiNET;
+
+namespace TopBases;
+
+public class TopBases : BaseSettingsPlugin<TopBasesSettings>
+{
+    private CachedValue<bool> ingameUIisVisible;
+
+    private static readonly ConcurrentDictionary<NormalInventoryItem, RectangleF> topBases =
+            new ConcurrentDictionary<NormalInventoryItem, RectangleF>();
+
+    public override void OnLoad()
+    {
+        CanUseMultiThreading = true;
+    }
+    public override bool Initialise()
+    {
+        var _ingameUI = GameController.IngameState.IngameUi;
+        ingameUIisVisible = new TimeCache<bool>(() =>
+            {
+                return _ingameUI.SyndicatePanel.IsVisibleLocal
+                    || _ingameUI.TreePanel.IsVisibleLocal
+                    || _ingameUI.Atlas.IsVisibleLocal;
+            }, 250);
+        return true;
+    }
+
+    public override Job Tick()
+    {
+        if (Settings.MultiThreading)
+            return GameController.MultiThreadManager.AddJob(TickLogic, Name);
+
+        TickLogic();
+        return null;
+    }
+
+    private void TickLogic()
+    {
+        var _playerInventory = GameController.Game.IngameState.IngameUi.InventoryPanel;
+        var _serverInventory = GameController.Game.IngameState.IngameUi.HaggleWindow;
+
+        var _topBasesList = new List<NormalInventoryItem>();
+
+        if (_playerInventory.IsVisibleLocal)
+        {
+            foreach (var item in _playerInventory[InventoryIndex.PlayerInventory].VisibleInventoryItems)
+            {
+                var BIT = GameController.Files.BaseItemTypes.Translate(item.Item.Path);
+                var _tags = BIT.Tags;
+                if (BIT == null) continue;
+                if (_tags.Contains("top_tier_base_item_type"))
+                    _topBasesList.Add(item);
+            }
+        }
+
+        if (_serverInventory.IsVisibleLocal)
+        {
+            foreach (var item in _serverInventory.InventoryItems)
+            {
+                var BIT = GameController.Files.BaseItemTypes.Translate(item.Item.Path);
+                var _tags = BIT.Tags;
+                if (BIT == null) continue;
+                if (_tags.Contains("top_tier_base_item_type"))
+                    _topBasesList.Add(item);
+            }
+        }
+
+        foreach (var key in topBases.Keys.Where(x => !_topBasesList.Contains(x)))
+            topBases.TryRemove(key, out _);
+
+        foreach (var item in _topBasesList)
+        {
+            var _rectangle = item?.GetClientRect();
+            if (_rectangle == null) continue;
+            topBases.AddOrUpdate(item, (RectangleF)_rectangle, (key, oldValue) => (RectangleF)_rectangle);
+        }
+    }
+
+    public override void Render()
+    {
+        if (ingameUIisVisible.Value) return;
+        var inventoryPanel = GameController.Game.IngameState.IngameUi.InventoryPanel;
+        var serverInventory = GameController.Game.IngameState.IngameUi.HaggleWindow;
+        foreach (var item in topBases.Keys)
+        {
+            var color = inventoryPanel.IsVisibleLocal && inventoryPanel[InventoryIndex.PlayerInventory].VisibleInventoryItems.Contains(item) ? Settings.InventoryColor.Value :
+                        serverInventory.IsVisibleLocal && serverInventory.InventoryItems.Contains(item) ? Settings.ServerColor.Value :
+                        Color.White;
+            Graphics.DrawFrame(topBases[item], color, Settings.BorderThickness);
+        }
+    }
+
+    public override void DrawSettings()
+    {
+        base.DrawSettings();
+        ImGui.Separator();
+    }
+
+
+}
---
+++ TopBasesSettings.cs
@@ -0,0 +1,28 @@
+﻿using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using SharpDX;
+
+namespace TopBases;
+
+public class TopBasesSettings : ISettings
+{
+    //Mandatory setting to allow enabling/disabling your plugin
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
+    public ToggleNode MultiThreading { get; set; } = new ToggleNode(false);
+    [Menu("If you don't want to draw items for a specific element, set the color transparency to 0", 100)]
+    public EmptyNode Description { get; set; }
+    [Menu("Color for the player inventory", parentIndex = 100)]
+    public ColorNode InventoryColor { get; set; } = new ColorNode(Color.White);
+    [Menu("Color for Rog/Gwennen inventory", parentIndex = 100)]
+    public ColorNode ServerColor { get; set; } = new ColorNode(Color.White);
+    [Menu("Border thickness", parentIndex = 100)]
+    public RangeNode<int> BorderThickness { get; set; } = new RangeNode<int>(1, 1, 5);
+
+
+
+    //Put all your settings here if you can.
+    //There's a bunch of ready-made setting nodes,
+    //nested menu support and even custom callbacks are supported.
+    //If you want to override DrawSettings instead, you better have a very good reason.
+}
